edfda9fb1ad28dc67c702dee07babc7371a3de6d
SMART24\User <fds44fs@nate.com>
2025-05-26 14:10:01 +0900

    first

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // ✅ .env 파일 로딩
+  await dotenv.load(fileName: '.env'); // 기본: .env
+
+  // ✅ Supabase 초기화
+  await Supabase.initialize(
+    url: dotenv.env['SUPABASE_URL']!,
+    anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+  );
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'To-Do 프로젝트',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      routes: {
+
+      },
+    );
+  }
+}
